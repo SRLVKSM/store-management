3393328717b8dd076495c8f2df7473765cf7799f
SRLVKSM <141706855+SRLVKSM@users.noreply.github.com>
2023-08-08 11:26:09 +0530

    Update userRoute.js
    Line 7 deleted

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/userController');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.post('/', userController.getUser);
+
 
 module.exports = router;
